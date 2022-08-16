ecf6ff20b20dca5874d7c05e530d906639624d68
banr1 <banri.yanahama@gmail.com>
2022-08-16 18:26:18 +0900

    add naive post

---
+++ README.md
@@ -0,0 +1,8 @@
+# Notion Automation
+
+`.env` example
+
+```
+NOTION_API_TOKEN=secret_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+NOTION_DATABASE_ID=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
-use std::collections::HashMap;
+use dotenv::dotenv;
+use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp =
-        reqwest::blocking::get("https://httpbin.org/ip")?.json::<HashMap<String, String>>()?;
-    println!("{:#?}", resp);
+    dotenv().ok();
+    let notion_api_token = env::var("NOTION_API_TOKEN")?;
+    let database_id = env::var("NOTION_DATABASE_ID")?;
+
+    let client = reqwest::blocking::Client::new();
+    let url = format!(
+        "https://api.notion.com/v1/databases/{database_id}/query",
+        database_id = database_id
+    );
+    let auth = format!(
+        "Bearer {notion_api_token}",
+        notion_api_token = notion_api_token
+    );
+
+    let resp = client
+        .post(url)
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .header(reqwest::header::AUTHORIZATION, auth)
+        .send()?;
+    let result = resp.text()?;
+    println!("{:#?}", result);
     Ok(())
 }
